936a1cb6d65699da64a981ae3615e90e3f4dce5e
wanhaotian <wanht23@mails.tsinghua.edu.cn>
2024-02-29 16:09:56 +0800

    Add death

---
+++ Assets/Scripts/Damageable.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+    
+    Animator animator;
+    public float _maxhealth=100;
+    public float Maxhealth
+    {
+        get
+        {
+            return _maxhealth;
+        }
+        set
+        {
+            _maxhealth = value;
+        }
+    }
+    public float _health = 100;
+    public float Health
+    {
+        get
+        {
+            return _health;
+        }
+        set
+        {
+            _health = value;
+            if (_health <= 0)
+            {
+                Isalive = false;
+            }
+        }
+    }
+
+    
+    public bool _isalive = true;
+   public bool isInvincible=false;
+    private float timesincehit=0;
+    private float invincibilityTimer=0.25f;
+
+    public bool Isalive
+    {
+        get
+        {
+            return _isalive;
+        }
+        set
+        {
+            _isalive = value;
+            animator.SetBool("isalive", value);
+            Debug.Log("Isalive set" + value);
+        }
+    }
+    
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInvincible)
+        {
+            if (timesincehit > invincibilityTimer)
+            {
+                isInvincible = false;
+                timesincehit = 0;
+            }
+            timesincehit += Time.deltaTime;
+        }
+        Hit(10);
+        
+    }
+    public void Hit(int damage)
+    {
+        if (Isalive && !isInvincible)
+        {
+            Health -= damage;
+            isInvincible = true;
+        }
+    }
+}
