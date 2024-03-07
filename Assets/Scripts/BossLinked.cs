9e2b3ede8762b24dbc45a2f1d0b3b5087e4dee82
wanhaotian <wanht23@mails.tsinghua.edu.cn>
2024-03-07 13:05:13 +0800

    Add Behaviour Tree

---
+++ Assets/Scripts/BossLinked.cs
@@ -0,0 +1,22 @@
+﻿using KadaXuanwu.UtilityDesigner.Scripts.Execution.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+    
+    public class ExampleReferences : SceneReferences
+    {
+        [SerializeField] private List<GameObject> gameObjects;
+        [SerializeField] private List<Transform> transforms;
+
+        protected override void RegisterCustomLists()
+        {
+            AddList(gameObjects);
+            AddList(transforms);
+        }
+    }
+
