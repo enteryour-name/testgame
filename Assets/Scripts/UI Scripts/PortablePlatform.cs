0745a1a5dbcf1622d201f8168db3fce71833c25f
wanhaotian <wanht23@mails.tsinghua.edu.cn>
2024-03-09 21:48:02 +0800

    1234

--- Assets/Scripts/UI Scripts/PortablePlatform.cs
+++ Assets/Scripts/UI Scripts/PortablePlatform.cs
@@ -12,9 +12,20 @@ public class PortablePlatform : MonoBehaviour
     public float speed;
     public float start;
     private float startoffset;
+    public Vector2 vec;
+    public Transform player;
+    public Transform platform;
+    public Rigidbody2D rigidbody2;
+    public CtrGenerateCharacter ctrGenerateCharacter;
+    public GameObject character1;
+    Rigidbody2D rb;
+   
     // Start is called before the first frame update
     void Start()
     {
+        character1 = ctrGenerateCharacter.character;
+        rb=character1.GetComponent<Rigidbody2D>();
+        rigidbody2=GetComponent<Rigidbody2D>();
         tr = GetComponent<Transform>();
         startPos = transform.position;
         start =3* Random.value;
@@ -24,6 +35,21 @@ public class PortablePlatform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position =startPos +new Vector3 (amplitude * Mathf.Sin((0.8f*speed+0.5f * startoffset) * (Time.time-startTime) + start) ,0,0);
+        vec= new Vector3(amplitude * Mathf.Sin((0.8f * speed + 0.5f * startoffset) * (Time.time - startTime) + start), 0,0);
+        platform.transform.position =startPos +new Vector3 (amplitude * Mathf.Sin((0.8f*speed+0.5f * startoffset) * (Time.time-startTime) + start) ,0,0);
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if ((true))
+        {
+            
+        }
+
+        if (collision.CompareTag("Player"))
+        {
+            
+            player.transform.position -= new Vector3(amplitude * Mathf.Sin((0.8f * speed + 0.5f * startoffset) * (Time.time - startTime) + start), 0, 0)*Time.deltaTime;
+           
+        }
     }
 }
