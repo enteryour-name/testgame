238edb2049c64bf56b4ea0e44ae1f44fa8692bb6
dby <dby23@mails.tsinghua.edu.cn>
2024-03-09 21:48:33 +0800

    try to fix TriggerSettings

--- Assets/Scripts/UI Scripts/TriggerSettings.cs
+++ Assets/Scripts/UI Scripts/TriggerSettings.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Cryptography;
 using UnityEngine;
 
@@ -7,6 +8,9 @@ public class TriggerSettings : MonoBehaviour
 {
     public GameObject enterBlock;
     public GameObject[] lastEnemies;
+    public GameObject[] enterTriggers;
+    public GameObject[] lastTriggers;
+    public GameObject[] antiDrops;
     public Vector3 returnPos= new Vector3(62,-29,0);
     public void EnterTrigger()
     {
@@ -26,15 +30,15 @@ public class TriggerSettings : MonoBehaviour
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.name == "EnterTrigger")
+        if (enterTriggers.Any(t => t==other.gameObject))
         {
             EnterTrigger();
         }
-        else if (other.name == "LastTrigger")
+        else if (lastTriggers.Any(t => t==other.gameObject))
         {
             LastTrigger();
         }
-        else if(other.name == "AntiDrop")
+        else if(antiDrops.Any(t => t==other.gameObject))
         {
             AntiDrop();
         }
